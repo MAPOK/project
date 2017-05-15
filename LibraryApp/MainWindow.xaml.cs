3c811ea1a6b618059ce0b4930968107a47b6ee44
anianiania <pacyk.anna@gmail.com>
2017-05-15 22:13:46 +0200

    Testowy przycisk

--- LibraryApp/MainWindow.xaml.cs
+++ LibraryApp/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace LibraryApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //test
+        }
     }
 }
