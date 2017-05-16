43f1c7c48d808095734c17c48d97a0e142c700f3
MiloszOkragly <milosz.okragly@gmail.com>
2017-05-16 21:08:35 +0200

    AccessDB Initial Commit

---
+++ LibraryWebApp/AccessDB/SQLTool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+
+namespace LibraryWebApp.AccessDB
+{
+    public class SQLTool
+    {
+        private OleDbConnection conn = null;
+
+        public SQLTool()
+        {
+            string conString = $"Provider=Microsoft.JET.OLEDB.4.0; data source={Path.Combine(Directory.GetCurrentDirectory(), "LibDB.mdb")}";
+            conn = new OleDbConnection(conString);
+        }
+    }
+}
